aca32fd38d603ffb1b2f11f6d06cbc0c6503a9ef
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-03-02 15:48:59 +0530

    Create fibs.c

---
+++ fibs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,n,t1=0,t2=1,nextterm;
+printf("enter the num term");
+scanf("%d",&n);
+printf("fibo series")
+{
+nextterm=t1+t2;
+t1=t2;
+t2=nextterm;
+printf("%d",t1);
+}
+}
